584fba4ad247f0fe823df58cf8e90b17abaf2554
Lyan <22127344@student.hcmus.edu.vn>
2024-11-25 13:58:33 +0700

    setup expressJS

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,1 +1,14 @@
-console.log("abh")
+import express from "express"
+
+const app = express();
+
+app.use(express.json())
+
+app.get("/a",(req,res) => {
+  console.log(req.body);
+  res.send("anh")
+})
+
+app.listen(3000, () => {
+  console.log("running on 3000")
+})

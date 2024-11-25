b165737dc7abaedf1084030c1dc10763b1083d3a
Lyan <22127344@student.hcmus.edu.vn>
2024-11-25 13:22:46 +0700

    reset backend folders

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,8 +1,1 @@
-import express from 'express'
-const app = express()
-app.use(express.json)
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-app.listen(3000)
-export default app
+console.log("abh")

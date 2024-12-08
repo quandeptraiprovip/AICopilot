0a346bf08288572dd35df515ae32785f7042d799
Zaciicia <quocanhnguyen874@gmail.com>
2024-12-08 07:34:16 +0700

    testing backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,14 +1,19 @@
 import express from "express"
 
+// const express = require("express");
+import dotenv from "dotenv";
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.json())
+// app.use('/a', import ('./routes/index.js'));
 
 app.get("/a",(req,res) => {
   console.log(req.body);
   res.send("anh quan")
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("running on 3000")
 })
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -2,6 +2,30 @@ import { Router } from "express"
 
 const appRouter = Router()
 
+
+appRouter.get("/", (req, res) => {
+  res.status(200).json({message: "routes/index.js get"})
+})
+
+appRouter.post("/:id", (req, res) => {
+    res.status(200).json({message: "routes/index.js post"})
+})
+
+appRouter.put("/:id", (req, res) => {
+    res.status(200).json({message: "routes/index.js put ${req.params.id}"})
+})
+
+appRouter.delete("/", (req, res) => {
+    res.status(200).json({message: "routes/index.js delete ${req.params.id}"})
+})
+module.exports = appRouter;
+
+
+
+
+
+
+
 appRouter.use("/user")
 
 export default appRouter;

842bbe06b1060497f8ec31e5f11296c541c96d16
Lyan <22127344@student.hcmus.edu.vn>
2024-12-31 18:13:55 +0700

    env file

--- src/backend/config/gemini.js
+++ src/backend/config/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI('AIzaSyCDxBTin3OsDqtCXbF_6Pquj7V7Zl14bAk');
+const genAI = new GoogleGenerativeAI(process.env.GEMINI);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const prompt = "Explain how AI works";
 const result = await model.generateContent(prompt);
